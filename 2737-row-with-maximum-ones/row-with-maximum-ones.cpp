5d55bdc6c64704456090ed22974407402be69388
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2026-03-16 22:38:52 +0530

    Time: 7 ms (30.55%) | Memory: 64.8 MB (36.91%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,21 +1,59 @@
 class Solution {
 public:
+
+    int noOfOnes(vector<int>&row, int n) {
+        if(n == 1)  return row[0] == 1;
+
+        int low = 0;
+        int high = n - 1;
+
+        sort(row.begin(), row.end());
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(row[mid] == 1)   
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return (n - 1) - low + 1;
+    }
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        vector<int>ans;
-        int maxi = INT_MIN;
-        for(int i=0; i<mat.size(); i++) {
-            int count = 0;
 
-            for(int j=0; j<mat[0].size(); j++) {
-                if(mat[i][j] == 1)
-                    count++;
-            }
+        // O(M * N)
+        // vector<int>ans;
+        // int maxi = INT_MIN;
+        // for(int i=0; i<mat.size(); i++) {
+        //     int count = 0;
 
-            if(count > maxi) {
-                maxi = count;
-                ans = {i, maxi};
+        //     for(int j=0; j<mat[0].size(); j++) {
+        //         if(mat[i][j] == 1)
+        //             count++;
+        //     }
+
+        //     if(count > maxi) {
+        //         maxi = count;
+        //         ans = {i, maxi};
+        //     }
+        // }
+        // return ans;
+
+        // O(M * log N)
+        int maxi = 0;
+        int indexOfMaxRow = 0;
+        int m = mat.size();
+        int n = mat[0].size();
+
+        for(int i=0; i<m; i++) {
+            int oneInThisRow = noOfOnes(mat[i], n);
+
+            if(oneInThisRow > maxi) {
+                maxi = oneInThisRow;
+                indexOfMaxRow = i;
             }
         }
-        return ans;
+        return {indexOfMaxRow, maxi};
+
     }
 };
