4615c6ca4bccb9bbb491baea3a925a3b7ac5aa73
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2026-04-05 11:28:57 +0530

    Time: 1 ms (60.99%) | Memory: 10.3 MB (47.26%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -3,15 +3,15 @@ public:
     bool judgeCircle(string moves) {
         if(moves.length() & 1)  return false;
 
-        int u = 0, d = 0, l = 0, r = 0;
+        int x = 0, y = 0;
 
-        for(auto c : moves) {
-            u += c == 'U';
-            d += c == 'D';
-            l += c == 'L';
-            r += c == 'R';
+        for (char c : moves) {
+            if (c == 'U') y++;
+            if (c == 'D') y--;
+            if (c == 'R') x++;
+            if (c == 'L') x--;
         }
 
-        return u == d && l == r;
+        return x == 0 && y == 0;
     }
 };
